82b4188243f08282de29615901867056e48be9e2
Shuo Ding <sdingcn@outlook.com>
2024-01-15 14:48:00 -0500

    clang-checks: error measurement script

--- extra-files/measure-error-stl.sh
+++
@@ -1,3 +0,0 @@
-clang++ -std=c++20 -E --no-line-commands stdtest.cc > prep.cc
-../build/bin/concept-synth prep.cc -- -std=c++20 > out.txt
-python3 measure-error.py prep.cc out.txt std
--- extra-files/measure-error.py
+++ extra-files/measure-error.py
@@ -1,17 +1,37 @@
 import sys
 import subprocess
+from typing import List, Tuple
 
-def read_code(s: str) -> ([str], str, str):
+def execute(cmd: List[str], i: str) -> Tuple[int, str, str]:
+    result = subprocess.run(
+        cmd,
+        text = True,
+        input = i,
+        capture_output = True
+    )
+    return (result.returncode, result.stdout, result.stderr)
+
+def preprocess(compiler: str, fpath: str, ipaths: List[str]) -> str:
+    cmd = [compiler, "-std=c++20", "-E", "-P", fpath]
+    for ipath in ipaths:
+        cmd.append("-I" + ipath)
+    result = execute(cmd)
+    if result[0] != 0:
+        sys.exit(f"Error: {compiler} failed on {fpath} during preprocessing")
+    return result[1]
+
+def synth(fpath: str) -> str:
+    result = execute(["../build/bin/concept-synth", fpath, "--", "-std=c++20"])
+    if result[0] != 0:
+        sys.exit(f"Error: concept-synth failed on {fpath}")
+    return result[1]
+
+def read_synth(s: str) -> Tuple[List[str], str, str]:
     _, tail1 = s.split("[-[Erroneous calls]-]")
     err_calls, tail2 = tail1.split("[-[Summary]-]")
     summary, tail3 = tail2.split("[-[Rewritten code]-]")
     rewritten, _ = tail3.split("[-[Resource consumption]-]")
-    ret1 = []
-    for call in err_calls.splitlines():
-        call = call.strip()
-        if call:
-            ret1.append(call)
-    return (ret1, rewritten.strip(), summary.strip())
+    return ([ec.strip() for ec in err_calls.splitlines() if ec.strip()], rewritten.strip(), summary.strip())
 
 def compose(main: str, call: str) -> str:
     lines = main.splitlines()
@@ -20,60 +40,51 @@ def compose(main: str, call: str) -> str:
     lines.insert(-1, call)
     return "\n".join(lines)
 
-def execute(cmd, i) -> str:
-    return subprocess.run(
-        cmd,
-        text = True,
-        input = i,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.STDOUT
-    ).stdout
-
-def approx(a: int, b: int) -> bool:
-    if a == 0 and b != 0:
-        return False
-    if a != 0 and b == 0:
-        return False
-    if a == 0 and b == 0:
-        return True
-    return abs(a - b) / min(a, b) < 0.05
+def run_compiler(compiler: str, code: str) -> Tuple[int, str, str]:
+    return execute([compiler, "-x", "c++", "-std=c++20", "-w", "-"], code)
 
 def get_callee_name(call: str) -> str:
     return call.split("(")[0]
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit(f"Usage: python3 {sys.argv[0]} <origin> <output> [name-prefix]")
-    print("[-[Starting script]-]")
-    with open(sys.argv[1], "r") as f1:
-        origin = f1.read().strip()
-    with open(sys.argv[2], "r") as f2:
-        err_calls, rewritten, summary = read_code(f2.read())
-    with open("rewritten.cc", "w") as f3:
-        f3.write(rewritten)
-    if len(sys.argv) == 4:
-        err_calls = [ec for ec in err_calls if ec.startswith(sys.argv[3])]
+def run_list(compiler: str, prep: str, rewritten: str, err_calls: List[str]) -> None:
+    print(f"[-[Starting error measurement {compiler}]-]")
     n = len(err_calls)
-    or_ne_tot, re_ne_tot, cnt = 0, 0, 0
+    pr_ne_tot, re_ne_tot = 0, 0
     err_lst = []
     for (i, call) in enumerate(err_calls):
-        origin_err = execute(["clang++", "-x", "c++", "-w", "-std=c++20", "-"], compose(origin, call))
-        rewritten_err = execute(["clang++", "-x", "c++", "-w", "-std=c++20", "-"], compose(rewritten, call))
-        err_lst.append((i + 1, origin_err, rewritten_err))
-        or_ne = len(origin_err.splitlines())
-        re_ne = len(rewritten_err.splitlines())
-        name = get_callee_name(call)
-        print(f"{i + 1}/{n}: or_ne = {or_ne},\tre_ne = {re_ne},\tname = {name}")
-        or_ne_tot += or_ne
+        pr_err = run_compiler(compiler, compose(prep, call))[2]
+        re_err = run_compiler(compiler, compose(rewritten, call))[2]
+        err_lst.append((i + 1, pr_err, re_err))
+        pr_ne = len(pr_err.splitlines())
+        re_ne = len(re_err.splitlines())
+        print(f"{i + 1}/{n}: pr_ne = {pr_ne},\tre_ne = {re_ne},\tname = {get_callee_name(call)}")
+        pr_ne_tot += pr_ne
         re_ne_tot += re_ne
-        cnt += 1
-    if cnt > 0:
-        print(f"or_ne_avg = {round(or_ne_tot / cnt, 3)},\tre_ne_avg = {round(re_ne_tot / cnt, 3)}")
-    print(summary)
-    with open("result", "w") as f4:
-        for num, or_err, re_err in err_lst:
-            f4.write(f"[-[{num}]-]{{{{{{\n")
-            f4.write(or_err + "\n")
-            f4.write("[>----------<]\n")
-            f4.write(re_err + "\n")
-            f4.write("}}}}}}\n\n\n\n\n\n")
+    if n > 0:
+        print(f"pr_ne_avg = {round(pr_ne_tot / n, 3)},\tre_ne_avg = {round(re_ne_tot / n, 3)}")
+    with open(compiler + "-errs.txt", "w") as f:
+        for i, pr_err, re_err in err_lst:
+            f.write(f"[-[{i}]-]{{{{{{\n")
+            f.write(pr_err + "\n")
+            f.write("[>------------------------------<]\n")
+            f.write(re_err + "\n")
+            f.write("}}}}}}\n\n\n\n\n\n")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit(f"Usage: python3 {sys.argv[0]} <fpath> [name-prefix]")
+    src_path = sys.argv[1]
+    for compiler in ["clang++-17", "g++-13"]:
+        print(f"[-[Starting script on {compiler}]-]")
+        prep_path = compiler + "-prep.cc"
+        with open(prep_path, w) as f1:
+            prep = preprocess(compiler, src_path, [])
+            f1.write(prep)
+        err_calls, rewritten, summary  = read_synth(synth(prep_path))
+        print(summary)
+        rewritten_path = compiler + "-rewritten.cc"
+        with open(rewritten_path, w) as f2:
+            f2.write(rewritten + "\n")
+        if len(sys.argv) == 3:
+            err_calls = [ec for ec in err_calls if ec.startswith(sys.argv[2])]
+        run_list(compiler, prep, rewritten, err_calls)
