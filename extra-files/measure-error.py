d3ebdf2d921b19b8c90c7a9f1f7c8ae3fc225492
Shuo Ding <sdingcn@outlook.com>
2024-01-15 13:26:09 -0500

    clang-checks: adjust summary

--- extra-files/measure-error.py
+++ extra-files/measure-error.py
@@ -1,17 +1,17 @@
 import sys
 import subprocess
 
-def read_code(s: str) -> ([str], str):
+def read_code(s: str) -> ([str], str, str):
     _, tail1 = s.split("[-[Erroneous calls]-]")
     err_calls, tail2 = tail1.split("[-[Summary]-]")
-    _, tail3 = tail2.split("[-[Rewritten code]-]")
+    summary, tail3 = tail2.split("[-[Rewritten code]-]")
     rewritten, _ = tail3.split("[-[Resource consumption]-]")
     ret1 = []
     for call in err_calls.splitlines():
         call = call.strip()
         if call:
             ret1.append(call)
-    return (ret1, rewritten.strip())
+    return (ret1, rewritten.strip(), summary.strip())
 
 def compose(main: str, call: str) -> str:
     lines = main.splitlines()
@@ -44,15 +44,15 @@ def get_callee_name(call: str) -> str:
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         sys.exit(f"Usage: python3 {sys.argv[0]} <origin> <output> [name-prefix]")
+    print("[-[Starting script]-]")
     with open(sys.argv[1], "r") as f1:
         origin = f1.read().strip()
     with open(sys.argv[2], "r") as f2:
-        err_calls, rewritten = read_code(f2.read())
+        err_calls, rewritten, summary = read_code(f2.read())
     with open("rewritten.cc", "w") as f3:
         f3.write(rewritten)
     if len(sys.argv) == 4:
         err_calls = [ec for ec in err_calls if ec.startswith(sys.argv[3])]
-    err_calls = err_calls[:30]
     n = len(err_calls)
     or_ne_tot, re_ne_tot, cnt = 0, 0, 0
     err_lst = []
@@ -69,6 +69,7 @@ if __name__ == '__main__':
         cnt += 1
     if cnt > 0:
         print(f"or_ne_avg = {round(or_ne_tot / cnt, 3)},\tre_ne_avg = {round(re_ne_tot / cnt, 3)}")
+    print(summary)
     with open("result", "w") as f4:
         for num, or_err, re_err in err_lst:
             f4.write(f"[-[{num}]-]{{{{{{\n")
